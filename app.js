4790e598c56a950287b587bea2009d527976be95
ANIL <anilarangi6@gmail.com>
2026-01-22 14:00:23 +0530

    Add ad

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-const lectures = [
-  {
-    day: "Day 1",
-    link: "https://drive.google.com/file/d/13YOI7df6t6sBUPvRmu2TVfbDUF6vIYTc/view?usp=sharing"
-  },
-  {
-    day: "Day 2",
-    link: "https://drive.google.com/file/d/1khaUDBmigvH0-Bsi-G-SxIli0qtkd5SX/view?usp=sharing"
-  },
-  {
-    day: "Day 3",
-    link: "https://drive.google.com/file/d/1Z9ngtO0YuSw7WE111mF01HTy9iwoIaWm/view?usp=sharing"
-  },
-  {
-    day: "Day 4",
-    link: "https://drive.google.com/file/d/1FaJ_-4UKukPgjb_eauSpUK91ydlc26DF/view?usp=sharing"
-  }
-];
+
 
 const grid = document.getElementById("lectureGrid");
 
